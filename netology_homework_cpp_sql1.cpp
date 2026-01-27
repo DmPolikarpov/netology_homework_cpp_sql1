01abadd77e024da1b3120b8794f6cefd1d2397d6
Dmitrii Polikarpov <dpolikarpov17@gmail.com>
2026-01-27 20:19:27 +0300

    initial commit

---
+++ netology_homework_cpp_sql1.cpp
@@ -0,0 +1,12 @@
+﻿// netology_homework_cpp_sql1.cpp: определяет точку входа для приложения.
+//
+
+#include "netology_homework_cpp_sql1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
